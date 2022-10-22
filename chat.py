9c1e620510d63feb3c8fb18707ccb310d1433bdd
Vapniak <Vapniak7@gmail.com>
2022-10-22 12:08:48 +0200

    Initial commit

---
+++ chat.py
@@ -0,0 +1,63 @@
+import random
+import json
+import torch
+import unicodedata
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r', encoding='utf8') as f:
+    intents = json.load(f)
+
+FILE = "data.pth"   
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+
+bot_name = "Nazwa Bota"
+
+def get_response(msg):
+    msg = unicodedata.normalize('NFKD', msg).encode('ascii', 'ignore')
+    msg = msg.decode('ascii')
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    #dostosowac prawdobodobienstwo
+    print(prob.item())
+    if prob.item() > 0.80:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    return "I do not understand..."
+
+
+if __name__ == "__main__":
+    while True:
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
---
+++ static/app.js
@@ -0,0 +1,91 @@
+class Chatbox {
+    constructor() {
+        this.args = {
+            //pobieranie class
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+        this.state = false;
+        this.messages = [];
+    }
+    display() {
+        const {chatBox, sendButton} = this.args;
+        //dodawanie przyciskow
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({key}) => {
+            //wysylanie pod przycikiem enter
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        //pokazywanie i ukrywanie chatboxa
+        if(this.state) {
+            chatbox.classList.add('chatbox--active')
+        } else {
+            chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value
+        if (text1 === "") {
+            return;
+        }
+
+        let msg1 = { name: "User", message: text1 }
+        this.messages.push(msg1);
+
+        //adres strony
+        fetch('/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          })
+          .then(r => r.json())
+          .then(r => {
+            //nazwac bota
+            let msg2 = { name: "Bob", message: r.answer };
+            this.messages.push(msg2);
+            this.updateChatText(chatbox)
+            textField.value = ''
+
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatbox)
+            textField.value = ''
+          });
+    }
+
+    updateChatText(chatbox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item, index) {
+            //wstawic taka sama nazwa jak nazwa bota
+            if (item.name === "Bob")
+            {
+                //dodawanie wiadomosci bota
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            }
+            else
+            {
+                //dodawanie wiadomosci uzytkownika
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            }
+          });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+const chatbox = new Chatbox();
+chatbox.display();
